fcd44beb481012baa736c8c76bf02d517c67b837
thewaywardlaureate <100750785+thewaywardlaureate@users.noreply.github.com>
2023-02-24 21:01:19 +0530

    Add files via upload

---
+++ 404.js
@@ -0,0 +1,28 @@
+const letters = "404ERROR";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+	  let iteration = 0;
+	  
+	  clearInterval(interval);
+	  
+	  interval = setInterval(() => {
+		      event.target.innerText = event.target.innerText
+		        .split("")
+		        .map((letter, index) => {
+				        if(index < iteration) {
+						          return event.target.dataset.value[index];
+						        }
+				      
+				        return letters[Math.floor(Math.random() * 6)]
+				      })
+		        .join("");
+		      
+		      if(iteration >= event.target.dataset.value.length){ 
+			            clearInterval(interval);
+			          }
+		      
+		      iteration += 1 / 3;
+		    }, 50);
+}
